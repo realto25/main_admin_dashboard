7c1bf07c96dd39a9571e0291aeb9dc0b12bcfb93
Abinanthan47 <abinanthan247@gmail.com>
2025-05-31 23:31:50 +0530

    fix feedback apiss

--- app/feedback/columns.tsx
+++ app/feedback/columns.tsx
@@ -4,7 +4,7 @@ import { ColumnDef } from "@tanstack/react-table";
 
 export type FeedbackColumn = {
   id: string;
-  bookingId: string;
+  visitRequestId: string;
   rating: number;
   experience: string;
   suggestions: string;
